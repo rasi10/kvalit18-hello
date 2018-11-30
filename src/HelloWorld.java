cb283fe04a09812d4a4ad55ca41a80ea72524d66
Rafael Silva <rasilva.1986@gmail.com>
2018-11-30 15:17:31 +0100

    this is my second version of this program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 
 public class HelloWorld {
+	
+	public void myMwethod1() {
+		System.out.println("this is a method that does not return a value!");
+	}
+	
+	public int myMethod2() {
+		System.out.println("This is a method that returns something");
+		return 1;
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
